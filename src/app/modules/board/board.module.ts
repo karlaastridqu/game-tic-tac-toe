c5805e913f4b9b04f1ab4d4ec3bb219a4b5344fe
Eric <ericzon@gmail.com>
2018-04-09 20:45:23 +0200

    ADD basic board & chip modules

---
+++ src/app/modules/board/board.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { BoardComponent } from './components/board/board.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [BoardComponent]
+})
+export class BoardModule { }
---
+++ src/app/modules/chip/chip.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ChipComponent } from './components/chip/chip.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [ChipComponent]
+})
+export class ChipModule { }
